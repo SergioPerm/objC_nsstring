60dcb04e41ca4ccb2d0e3c81d98112e5996cf68d
Admin <admin@mac-admin.local>
2018-06-13 19:12:08 +0300

    Initial Commit

---
+++ NSStringTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSStringTest
+//
+//  Created by Admin on 07.06.2018.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSStringTestTests/NSStringTestTests.m
@@ -0,0 +1,39 @@
+//
+//  NSStringTestTests.m
+//  NSStringTestTests
+//
+//  Created by Admin on 13.06.2018.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSStringTestTests : XCTestCase
+
+@end
+
+@implementation NSStringTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
